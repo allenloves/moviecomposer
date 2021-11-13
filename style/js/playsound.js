b8cb3c16ad441db71e59899058afb4f9edc8fa69
Allen SC Wu <allenloves@gmail.com>
2021-11-13 13:02:12 +0800

    music player progress bar

--- style/js/playsound.js
+++ style/js/playsound.js
@@ -1,16 +1,29 @@
-var notPlaying = true, playingSound = 0, playingID = 0;
+var notPlaying = true, playingSound = 0, playingID = 0, progressID = 0, 
+	playingProgress = 0, playingProgressClick = 0;
+
 //var playingID = void;
 
 
 
 function play_single_sound(id) {
-	if(notPlaying){ //start playing
-			playingSound = document.getElementById(id);
+	if(notPlaying){ //start playing	
+			if(playingID != id && playingID != 0){
+				playingSound.currentTime = 0;
+				playingProgress.style.width = `0%`;
+			}
 			playingID = id;
+			progressID = id+"progress";
+			clickID = id+"click";
+			playingSound = document.getElementById(playingID);
+			playingProgress = document.getElementById(progressID);
+			playingProgressClick = document.getElementById(clickID);
+			playingSound.addEventListener('timeupdate', updateProgress);
+			playingSound.addEventListener('ended', resetSound);
+			playingProgressClick.addEventListener('click', setProgress);
 			playingSound.play();
 			notPlaying = false;
 		}
-	else {
+	else { //while there is music playing
 			playingSound.pause();
 			if(playingID == id){  //click to pause
 				//playingSound.currentTime = 0;  //rewind to beginning
@@ -18,11 +31,37 @@ function play_single_sound(id) {
 			}
 			else { //click another piece
 				playingSound.currentTime = 0;
-				playingSound = document.getElementById(id);
+				playingProgress.style.width = `0%`
 				playingID = id;
+				progressID = id+"progress";
+				clickID = id+"click";
+				playingSound = document.getElementById(playingID);
+				playingProgress = document.getElementById(progressID);
+				playingProgressClick = document.getElementById(clickID);
+				playingSound.addEventListener('timeupdate', updateProgress);
+				playingSound.addEventListener('ended', resetSound);
+				playingProgressClick.addEventListener('click', setProgress);
 				playingSound.play();
 			}
 		}
-	}
+}
 
+function updateProgress(e) {
+	const {duration, currentTime} = playingSound;
+	const progressPercent = (currentTime / duration) * 100;
+	playingProgress.style.width = `${progressPercent}%`;
+}
 
+function setProgress(e) {
+	const width = this.clientWidth;
+	const clickX = e.offsetX;
+	const duration = playingSound.duration;
+
+	playingSound.currentTime = (clickX / width) * duration;
+}
+
+function resetSound(e) {
+	playingSound.pause();
+	notPlaying = true;
+	playingSound.currentTime = 0;
+}
